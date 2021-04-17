33bcd0117d6f712bae06bd77019d24da5c0b4f39
AdamBOD <adambarryodonovan@gmail.com>
2021-04-18 00:47:39 +0100

    Updated User table and added UserImage table

---
+++ VersionSQL/DESKTOP-0HGMMN7/Shufl/dbo/FunctionScalar/fnGetVersion.sql
@@ -0,0 +1,15 @@
+﻿/****** Object:  Function [dbo].[fnGetVersion]    Committed by VersionSQL https://www.versionsql.com ******/
+
+-- =============================================
+-- Author:		Adam Barry-O'Donovan
+-- Create date: 17/04/2021
+-- Description:	Function to return version number
+-- =============================================
+CREATE FUNCTION fnGetVersion ()
+RETURNS varchar
+AS
+BEGIN
+
+	RETURN '1.1.0'
+
+END
---
+++ VersionSQL/DESKTOP-0HGMMN7/Shufl/dbo/Table/UserImage.sql
@@ -0,0 +1,20 @@
+﻿/****** Object:  Table [dbo].[UserImage]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[UserImage](
+	[Id] [uniqueidentifier] NOT NULL,
+	[UserId] [uniqueidentifier] NOT NULL,
+	[Width] [smallint] NULL,
+	[Height] [smallint] NULL,
+	[Uri] [char](40) NOT NULL,
+ CONSTRAINT [PK_UserImage] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+ALTER TABLE [dbo].[UserImage] ADD  CONSTRAINT [DF_UserImage_Id]  DEFAULT (newsequentialid()) FOR [Id]
+ALTER TABLE [dbo].[UserImage]  WITH CHECK ADD  CONSTRAINT [FK_UserImage_User] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([Id])
+ALTER TABLE [dbo].[UserImage] CHECK CONSTRAINT [FK_UserImage_User]
