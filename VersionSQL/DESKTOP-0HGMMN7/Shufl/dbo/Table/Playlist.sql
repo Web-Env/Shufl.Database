6a105bb46a5a30fec1d8207e1c5b2f59fcae9ce8
AdamBOD <adambarryodonovan@gmail.com>
2021-05-17 20:57:34 +0100

    Added Playlist tables (#9)

---
+++ VersionSQL/DESKTOP-0HGMMN7/Shufl/dbo/Table/Playlist.sql
@@ -0,0 +1,31 @@
+﻿/****** Object:  Table [dbo].[Playlist]    Committed by VersionSQL https://www.versionsql.com ******/
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+CREATE TABLE [dbo].[Playlist](
+	[Id] [uniqueidentifier] NOT NULL,
+	[SpotifyId] [char](22) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[CreatedBy] [uniqueidentifier] NOT NULL,
+	[LastUpdatedOn] [datetime] NOT NULL,
+	[LastUpdatedBy] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [PK_Playlist] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+SET ANSI_PADDING ON
+
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Playlist_SpotifyId] ON [dbo].[Playlist]
+(
+	[SpotifyId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+ALTER TABLE [dbo].[Playlist] ADD  CONSTRAINT [DF_Playlist_Id]  DEFAULT (newsequentialid()) FOR [Id]
+ALTER TABLE [dbo].[Playlist]  WITH CHECK ADD  CONSTRAINT [FK_Playlist_User_CreatedBy] FOREIGN KEY([CreatedBy])
+REFERENCES [dbo].[User] ([Id])
+ALTER TABLE [dbo].[Playlist] CHECK CONSTRAINT [FK_Playlist_User_CreatedBy]
+ALTER TABLE [dbo].[Playlist]  WITH CHECK ADD  CONSTRAINT [FK_Playlist_User_LastUpdatedBy] FOREIGN KEY([LastUpdatedBy])
+REFERENCES [dbo].[User] ([Id])
+ALTER TABLE [dbo].[Playlist] CHECK CONSTRAINT [FK_Playlist_User_LastUpdatedBy]
