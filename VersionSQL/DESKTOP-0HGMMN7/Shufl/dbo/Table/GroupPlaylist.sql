a3376f4002c105fff5c11a5465c51bb4dd96f263
AdamBOD <adambarryodonovan@gmail.com>
2021-05-17 21:09:05 +0100

    Added Group FK to GroupPlaylist

--- VersionSQL/DESKTOP-0HGMMN7/Shufl/dbo/Table/GroupPlaylist.sql
+++ VersionSQL/DESKTOP-0HGMMN7/Shufl/dbo/Table/GroupPlaylist.sql
@@ -30,6 +30,9 @@ CREATE NONCLUSTERED INDEX [IX_GroupPlaylist_GroupId_PlaylistId] ON [dbo].[GroupP
 	[PlaylistId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ALTER TABLE [dbo].[GroupPlaylist] ADD  CONSTRAINT [DF_GroupPlaylist_Id]  DEFAULT (newsequentialid()) FOR [Id]
+ALTER TABLE [dbo].[GroupPlaylist]  WITH CHECK ADD  CONSTRAINT [FK_GroupPlaylist_Group] FOREIGN KEY([GroupId])
+REFERENCES [dbo].[Group] ([Id])
+ALTER TABLE [dbo].[GroupPlaylist] CHECK CONSTRAINT [FK_GroupPlaylist_Group]
 ALTER TABLE [dbo].[GroupPlaylist]  WITH CHECK ADD  CONSTRAINT [FK_GroupPlaylist_Playlist] FOREIGN KEY([PlaylistId])
 REFERENCES [dbo].[Playlist] ([Id])
 ALTER TABLE [dbo].[GroupPlaylist] CHECK CONSTRAINT [FK_GroupPlaylist_Playlist]
